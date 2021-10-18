5e008e0bb50cd754348daf8050d5a5d8295243b1
Nick White <bbycinka@gmail.com>
2021-10-18 16:27:29 +0300

    Добавлен axios

--- js/script.js
+++ js/script.js
@@ -191,6 +191,12 @@ window.addEventListener('DOMContentLoaded', function() {
         return await res.json();
     };
 
+    // axios.get('http://localhost:3000/menu')
+    // .then( data => {
+    //     data.forEach( ({img,altimg,title,descr,price}) =>{
+    //         new MenuCard(img,altimg,title,descr,price, '.menu .container').render();
+    // });
+
     // getResource('http://localhost:3000/menu')
     // .then(data => {
     //     data.forEach( ({img,altimg,title,descr,price}) =>{
